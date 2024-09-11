fed7b80ad35e6325844f7c22fc7ac21e6d2c2737
roma <roman4uni@gmail.com>
2024-09-11 13:41:58 -0300

    new main

--- warping/src/utils.cpp
+++ warping/src/utils.cpp
@@ -21,6 +21,26 @@ bool BoundingBox::contiene(glm::vec3 &p) const {
 Pesos calcularPesos(glm::vec3 x0, glm::vec3 x1, glm::vec3 x2, glm::vec3 x) {
 	/// @todo: implementar
 	
+	// Alt
+	
+	//	glm::vec3 pcA_1 = x1-x0;
+	//	glm::vec3 pcA_2 = x2-x0;
+	//	glm::vec3 A = cross(pcA_1,pcA_2);
+	//	
+	//	glm::vec3 pcx0 = x0-x;
+	//	glm::vec3 pcx1 = x1-x;
+	//	glm::vec3 pcx2 = x2-x;
+	//	
+	//	glm::vec3 A0 = cross(pcx1,pcx2);
+	//	glm::vec3 A1 = cross(pcx2,pcx0);
+	//	glm::vec3 A2 = cross(pcx0,pcx1);
+	//	
+	//	float peso_0 = dot(A0,A)/dot(A,A);
+	//	float peso_1 = dot(A1,A)/dot(A,A);
+	//	float peso_2 = dot(A2,A)/dot(A,A);
+	//	
+	//	return {peso_0,peso_1,peso_2};
+	
 	//Area total.
 	glm::vec3 at = cross((x1 - x0),(x2 - x0));
 	float at2 = dot(at,at); //at²
@@ -37,6 +57,7 @@ Pesos calcularPesos(glm::vec3 x0, glm::vec3 x1, glm::vec3 x2, glm::vec3 x) {
 	glm::vec3 a2 = cross((x0 - x),(x1 - x));
 	float alpha2 = dot(a2,at)/at2;
 	
+
 	//debug
 //	std::cout<<dot(a1,at)<<"/"<<at2<<std::endl;
 //	std::cout<<glm::to_string(a1)<< " "<<glm::to_string(at)<<std::endl;
