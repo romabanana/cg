cfdc34c2ef8b990bf70be7d7283827bf570cdf91
roma <roman4uni@gmail.com>
2024-11-21 22:16:59 -0300

    h

--- subdiv/src/main_q.cpp
+++ subdiv/src/main_q.cpp
@@ -13,10 +13,9 @@
 #include "Shaders.hpp"
 #include "SubDivMesh.hpp"
 #include "SubDivMeshRenderer.hpp"
-
-#define VERSION 20241025
 #include <iostream>
-//using namespace std;
+#include <utility>
+#define VERSION 20241025
 
 // models and settings
 std::vector<std::string> models_names = { "cubo", "icosahedron", "plano", "suzanne", "star" };
@@ -136,6 +135,9 @@ struct Arista {
 	const bool operator<(const Arista &a) const {
 		return (n[0]<a.n[0]||(n[0]==a.n[0]&&n[1]<a.n[1]));
 	}
+	const bool operator==(const Arista &a) const {
+		return (n[0]==a.n[0]&&n[1]<a.n[1]);
+	}
 };
 
 // Mapa sirve para guardar una asociación entre una arista y un indice de nodo (que no es de la arista)
@@ -146,25 +148,25 @@ void subdivide(SubDivMesh &mesh) {
 	/// @@@@@: Implementar Catmull-Clark... lineamientos:
 	//  Los nodos originales estan en las posiciones 0 a #n-1 de m.n,
 	//  Los elementos orignales estan en las posiciones 0 a #e-1 de m.e
+	
+	int cant_nodos_originales = mesh.n.size();	///Cantidad nodos inicial
+	
 	//  1) Por cada elemento, agregar el centroide (nuevos nodos: #n a #n+#e-1)
-//	std::vector<Elemento> element_vector = mesh.e;
-//	std::vector<Nodo> node_vector = mesh.n;
-//	int original_size = node_vector.size();
-
-	int size_original = mesh.n.size();
 	
-	glm::vec3 p = glm::vec3 (0,0,0);
-	Nodo nuevo_nodo = Nodo(p);
-	
-	for(Elemento elem : mesh.e){
-		p = glm::vec3 (0,0,0);
-		int cant = elem.nv;
-		for(size_t i = 0; i < cant; i++){
-			p += mesh.n[elem.n[i]].p;
+	///Calculo los centroides de un elemento sumando los nodos de dicho Elemento 
+	///y lo agrego al vector de nodos; el Elemento[i] tendra un nodo(centroide)[i+n] asociado 
+	for(int i=0;i<mesh.e.size();i++){
+		
+		int cant_nodos = mesh.e[i].nv;
+		
+		glm::vec3 sum_nodos(0.f,0.f,0.f);
+		for(int j=0;j<cant_nodos;j++){
+			int ind_nodo = mesh.e[i][j];
+			sum_nodos += mesh.n[ind_nodo].p;
 		}
-		p = p/float(cant);
-		nuevo_nodo = Nodo(p);
-		mesh.n.push_back(nuevo_nodo);
+		
+		glm::vec3 centroide_i = sum_nodos/(cant_nodos*1.f);
+		mesh.n.push_back(Nodo(centroide_i));
 	}
 	
 	//  2) Por cada arista de cada cara, agregar un pto en el medio que es
@@ -177,51 +179,39 @@ void subdivide(SubDivMesh &mesh) {
 	//      arista, y ya que no se pueden relacionar los indices con una formula simple
 	//      se sugiere usar Mapa como estructura auxiliar
 	
-	Mapa mapita;
-	p = glm::vec3 (0,0,0);
-	int i_aristas = 0;
-	int i_elemento = 0;
+	///Arista = dos indices de los nodos que la forman, ordenados de menor a mayor
+	///Con el mapa se le asocia el nodo de arista correspondiente
+	std::map<Arista,int> aristas;
 	
-	for(Elemento elem: mesh.e){
-		int nodos = elem.nv;
-		for(size_t i = 0; i < nodos; i++){ 
-			glm::vec3 p = glm::vec3 (0,0,0);
-			int n1 = elem.n[i];
-			int n2 = elem.n[(i+1)%nodos];
+	///Guardo cada arista (su par de nodos) y el nodo correspondiente a esta
+	for(int i=0;i<mesh.e.size();i++){
+		int cant_nodos_por_elemento = mesh.e[i].nv;
+		for(int j=0;j<cant_nodos_por_elemento;j++){
+			///Como los nodos de un elemento estan ordenados en sentido contrareloj, el nodo[j] del elemento[i] y el siguiente nodo[(j+1)%nv] del elemento[i] forman una arista
+			int n1 = mesh.e[i].n[j];
+			int n2 = mesh.e[i].n[(j+1)%cant_nodos_por_elemento];
 			
-			Arista a(n1,n2);
+			Arista arista_i(n1,n2);
 			
-			if(mapita.find(a)== mapita.end()){
+			if(aristas.find(arista_i)==aristas.end()){	///Si nunca tuve en cuenta esta arista entonces calculo el nodo de arista correspondiente y agrego ambos a mi mapa de aristas
 				
-				///Agregar arista al mapa
-				mapita[a]=i_aristas; 
-
-				i_aristas++;				
-				p = mesh.n[n1].p + mesh.n[n2].p;
-				p = p*0.5f;
+				glm::vec3 punto_medio = (mesh.n[n1].p + mesh.n[n2].p)/2.f;	///Si la arista es frontera, el nodo de arista es el promedio de los nodos que forman dicha arista (es decir el punto medio de la arista)
 				
-				///verificar frontera;
-				if(elem.v[i] != -1){ // no frontera
-					glm::vec3 centroide = mesh.n[size_original+i_elemento].p;
-					glm::vec3 centroide_vecino = mesh.n[size_original+elem.v[i]].p;
+				if( mesh.e[i].v[j]!=-1){	///Pero si la arista no es frontera (si tiene elementos vecinos) es el promedio de los nodos que la forman y los centroides de los elementos que comparten la arista
+												///La arista de los nodos n[j] y n[j+1] del elemento e[i] tiene un vecino e[i].v[j]
 					
-					glm::vec3 prom_c = (centroide + centroide_vecino)*0.5f;
-					p += prom_c; 
-					p = p*0.5f;
-					
+					glm::vec3 f1 = mesh.n[i+cant_nodos_originales].p;
+					int ind_vecino = (mesh.e[i].v[j]);
+					glm::vec3 f2 = mesh.n[ind_vecino+cant_nodos_originales].p;
+					punto_medio = (punto_medio + (f1 + f2)/2.f)/2.f;
 				}
 				
-				nuevo_nodo = Nodo(p);
-				mesh.n.push_back(nuevo_nodo);
-				}
+				aristas.insert({arista_i,mesh.n.size()});
+				mesh.n.push_back(Nodo(punto_medio));
+				
 			}
-			i_elemento++;
 		}
-	
-//	for (const auto& entry : mapita) {
-//		std::cout << "Key: (" << entry.first.n[0] << ", " << entry.first.n[1] << ")" << std::endl;
-//	}
-
+	}
 	
 	//  3) Armar los elementos nuevos
 	//      Los quads se dividen en 4, (uno reemplaza al original, los otros 3 se agregan)
@@ -229,41 +219,52 @@ void subdivide(SubDivMesh &mesh) {
 	//      Para encontrar los nodos de las aristas usar el mapa que armaron en el paso 2
 	//      Ordenar los nodos de todos los elementos nuevos con un mismo criterio (por ej, 
 	//      siempre poner primero al centroide del elemento), para simplificar el paso 4.
-
+	int cant_elementos = mesh.e.size();	///Cantidad elementos inicial
 	
-	int elementos = mesh.e.size();
+	///Armo los nuevos elementos (uno por cada nodo[j] del elemento[i] (excepto uno para modificar el original)) 	///Se recorre el vector de nodos del elemento y no el vector de nodos del mesh
+	///Cada elemento guarda sus nodos de la manera (notar que mantiene el sentido de recorrido contrareloj): 
+	///	n[0] = centroide del elemento[i] original
+	///	n[1] = nodo de arista entre n[j] del elemento[i] y n[(j-1+nv)%nv] del elemento[i] (es decir del nodo j del elemento original y el anterior) 
+	///	n[2] = nodo e[i].n[j]
+	///	n[3] = nodo de arista entre n[j] del elemento[i ] y n[(j+1)%nv] del elemento[i] (es decir del nodo j del elemento original y el posterior)
 	
-	for(size_t i = 0; i<elementos; i++){
-		// un elemente se construye(n1,n2,n3,n4);
-		int nodos = mesh.e[i].nv;
-		int n0 = size_original+i; //centroide
+	for(int i=0; i<cant_elementos;i++){
 		
-		for(size_t j = 0; j<nodos; j++){
+		int cant_nodos_por_elemento = mesh.e[i].nv;
+		int ind_centroide = cant_nodos_originales+i;
+		
+		for(int j=0;j<cant_nodos_por_elemento-1;j++){
 			
-			int n2 = mesh.e[i].n[j]; // Nodo original
-			int next = mesh.e[i].n[(j+1)%nodos]; // Siguiente en el elemento
-			int prev = mesh.e[i].n[(j-1+nodos)%nodos]; // previo en el elemento
+			int j_nodo = mesh.e[i].n[j];
 			
-			int i1 = mapita[Arista(n2, prev)]; // indice asociado a arista previa
-			int i3 = mapita[Arista(n2, next)]; // indice asociado a arista siguiente
+			int n_a1 = mesh.e[i].n[ (j+1) % cant_nodos_por_elemento];
+			int n_a2 = mesh.e[i].n[ (j-1+cant_nodos_por_elemento)  % cant_nodos_por_elemento];
 			
-			int n1 = size_original+elementos+i1; // indice de nodo1
-			int n3 = size_original+elementos+i3; // indice de nodo3
+			Arista a1(j_nodo,n_a1);
 			
-			if(j==nodos-1){
-				mesh.reemplazarElemento(i,n0,n1,n2,n3);
-			}else{
-				mesh.agregarElemento(n0,n1,n2,n3);
-				//n0 : centroide
-				//n1 : n. arista ste.
-				//n2 : n. original.
-				//n3 : n. arista pre.
-			}
+			Arista a2(j_nodo,n_a2);
+			
+			int c1 = aristas[a1];
+			int c2 = aristas[a2];
+			
+			mesh.agregarElemento(ind_centroide,c2,j_nodo,c1);///Ademas de armar los elementos se encarga de actualizar el vector e de cada nodo n
 			
 		}
 		
+		int j_ult = mesh.e[i].n[cant_nodos_por_elemento-1];
+		int n_a1 = mesh.e[i].n[0];
+		int n_a2 = mesh.e[i].n[cant_nodos_por_elemento-2];
+		
+		Arista a1(j_ult,n_a1);
+		Arista a2(j_ult,n_a2);
+		
+		int c1 = aristas[a1];
+		int c2 = aristas[a2];
+		
+		mesh.reemplazarElemento(i,ind_centroide,c2,j_ult,c1);///Ademas de modificar el elemento se encarga de actualizar el vector e de cada nodo n
 	}
-	mesh.makeVecinos();
+	
+	mesh.makeVecinos();///Actualiza el vector de vecinos de los elementos (vacio en los nuevos y erroneo en el actualizado) y el bool es_frontera de cada nodo
 	
 	//  4) Calcular las nuevas posiciones de los nodos originales
 	//      Para nodos interiores: (4r-f+(n-3)p)/n
@@ -277,78 +278,63 @@ void subdivide(SubDivMesh &mesh) {
 	//      Ojo: en el paso 3 cambio toda la SubDivMesh, analizar donde quedan en los nuevos 
 	//      elementos (¿de que tipo son?) los nodos de las caras y los de las aristas 
 	//      que se agregaron antes.
-	
-	std::vector<int> visitados;
-	for(size_t i = 0; i<size_original; i++){
-//		std::cout<<"idx :"<<i+1<<"/"<<size_original<<std::endl;
-	
-		//f:promedio de centroides
-		//f:promedio de pts asociados a r istas
-		glm::vec3 f = glm::vec3(0,0,0);
-		glm::vec3 r = glm::vec3(0,0,0);
-		glm::vec3 nodo_original = mesh.n[i].p;
-		
-		int cant_asociados = mesh.n[i].e.size();//n
-
-		for(size_t j = 0; j<cant_asociados; j++){
-			int idx_e = mesh.n[i].e[j]; //indice del elem
-			int idx_c = mesh.e[idx_e].n[0]; //indice centroide
-			f += mesh.n[idx_c].p; //centroide
-		}
-		f = f/float(cant_asociados);
-		
-		int cant_aristas = 0;
-		
-		for(size_t j = 0; j<cant_asociados; j++){
-			int idx_e = mesh.n[i].e[j]; //indice del elem
-			
-			int n1 = mesh.e[idx_e].n[1];
-			int n3 = mesh.e[idx_e].n[3];
-//			std::cout<<"n1: "<<n1<<", n3:"<<n3<<std::endl;
-			if(std::find(visitados.begin(),visitados.end(),n1)==visitados.end()){
-					visitados.push_back(n1);
-					r += mesh.n[n1].p;
-					cant_aristas++;
-			}
-						
-			if(std::find(visitados.begin(),visitados.end(),n3)==visitados.end()){
-					visitados.push_back(n3);
-					r += mesh.n[n3].p;
-					cant_aristas++;
-			}
-		}
-		
-		std::cout<< cant_aristas<<std::endl;
-		r = r/float(cant_aristas);
-		std::cout << visitados.size() << std::endl;
-		visitados.clear();
-		
-		if(mesh.n[i].es_frontera){
-			mesh.n[i].p = (r+nodo_original)/2.f;
-		}else{
-			mesh.n[i].p = ((4.f*r) - f + float((cant_asociados-3.f))*nodo_original)/float(cant_asociados);
-		}
-//		mesh.n[i] = node;
-		
-	}
-		
-	
 	// tips:
 	//   no es necesario cambiar ni agregar nada fuera de este método, (con Mapa como 
 	//     estructura auxiliar alcanza)
-	//   sugerencia: probar primero usando el cubo (es cerrado y solo tiene quads)
-	//               despues usando la piramide (tambien cerrada, y solo triangulos)
-	//               despues el ejemplo plano (para ver que pasa en los bordes)
-	//               finalmente el mono (tiene mezcla y elementos sin vecinos)
-	//   repaso de como usar un mapa:
-	//     para asociar un indice (i) de nodo a una arista (n1-n2): elmapa[Arista(n1,n2)]=i;
-	//     para saber si hay un indice asociado a una arista:  ¿elmapa.find(Arista(n1,n2))!=elmapa.end()?
-	//     para recuperar el indice (en j) asociado a una arista: int j=elmapa[Arista(n1,n2)];
+
+	///Calculo las posiciones de los nodos[0:n-1] del mesh
+	for(int j=0;j<cant_nodos_originales;j++){
+		
+		int cant_elementos_por_nodo = mesh.n[j].e.size();
+		
+		///Calculo el promedio de los centroides de los elementos asociados al nodo (primer nodo (n[0]) del elemento)
+		glm::vec3 f(0.f,0.f,0.f);
+		for(int i=0;i<cant_elementos_por_nodo;i++){
+			int ind_elemento = mesh.n[j].e[i];
+			int ind_centroide = mesh.e[ind_elemento].n[0];
+			f+= mesh.n[ind_centroide].p;
+		}
+		f=f/(cant_elementos_por_nodo*1.f);
+		
+		///Calculo el promedio de los nodos arista asociados al nodo original
+		glm::vec3 r(0.f,0.f,0.f);
+		std::vector<int> nodos_arista;
+		
+		///Busqueda
+		for(int i=0;i<cant_elementos_por_nodo;i++){ 
+			int ind_elemento = mesh.n[j].e[i];
+			
+			///Si el nodo es frontera: tengo en cuenta solo las aristas frontera que son las que tienen ambos nodos frontera (es decir si el nodo con el que forma la arista tambien es frontera)
+			///Si no es frontera: tengo en cuenta todas las aristas asociadas al nodo
+			
+			int ind_arista_1 = mesh.e[ind_elemento].n[1]; 
+			if(find(nodos_arista.begin(),nodos_arista.end(),ind_arista_1)==nodos_arista.end() and (mesh.n[ind_arista_1].es_frontera or !mesh.n[j].es_frontera))	nodos_arista.push_back(ind_arista_1);
+			
+			int ind_arista_2 = mesh.e[ind_elemento].n[3]; 
+			if(find(nodos_arista.begin(),nodos_arista.end(),ind_arista_2)==nodos_arista.end() and (mesh.n[ind_arista_2].es_frontera or !mesh.n[j].es_frontera))	nodos_arista.push_back(ind_arista_2);
+		}	
+		
+		///Calculo promedio nodos arista
+		for(int i=0;i<(nodos_arista.size());i++){
+			r+= mesh.n[nodos_arista[i]].p;
+		};
+		r=r/(nodos_arista.size()*1.f);
+		
+		if(mesh.n[j].es_frontera){
+			mesh.n[j].p = (r+mesh.n[j].p)/2.f; ///Si es frontera calculo el promedio entre nodos arista y el nodo original
+		}else {
+			mesh.n[j].p = (4.f*r-f+(cant_elementos_por_nodo-3.f)*mesh.n[j].p)/(cant_elementos_por_nodo*1.f);///Si no es frontera calculo el promedio ponderado afin (si sumo los pesos y los dividido por la cant_elementos del nodo estos suman 1)
+		}
+	}
 	
 	
 	// Esta llamada valida si la estructura de datos quedó consistente (si todos los
 	// índices están dentro del rango válido, y si son correctas las relaciones
 	// entre los .n de los elementos y los .e de los nodos). Mantener al final de
 	// esta función para ver que la subdivisión implementada no rompa esos invariantes.
+	
 	mesh.verificarIntegridad();
+	/// ve para cada nodo de un elemento, que el nodo tenga la ref al elemento en su e	
+	/// y ve que cada elemento al que un nodo dice pertenecer, efectivamente lo contenga
+
 }
